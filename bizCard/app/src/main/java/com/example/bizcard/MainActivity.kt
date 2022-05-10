135670fac9d27337db96899fd980eca16b9c43f7
BartoszRudnik <rudnik49@gmail.com>
2022-05-10 15:39:48 +0200

    init commit

---
+++ bizCard/app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.bizcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bizcard.ui.theme.BizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BizCardTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CreateBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateBizCard() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BizCardTheme {
+        CreateBizCard()
+    }
+}
