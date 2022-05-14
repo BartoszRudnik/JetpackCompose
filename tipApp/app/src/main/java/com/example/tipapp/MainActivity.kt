c8b39691ec49dd5e8878fcb964ba95b9bd6690ed
BartoszRudnik <rudnik49@gmail.com>
2022-05-14 16:41:57 +0200

    tipApp -> create textfield for bill amount

--- tipApp/app/src/main/java/com/example/tipapp/MainActivity.kt
+++ tipApp/app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -7,18 +7,24 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.tipapp.components.InputField
 import com.example.tipapp.ui.theme.TipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,16 +50,45 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Preview
 @Composable
 fun MainContent() {
+    BillForm() { billAmount ->
+        {}
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun BillForm(modifier: Modifier = Modifier, onValChange: (String) -> Unit = {}) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Surface(
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(2.dp, Color.LightGray)
     ) {
         Column() {
-            
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) {
+                        return@KeyboardActions
+                    }
+                    onValChange(totalBillState.value.trim())
+
+                    keyboardController?.hide()
+                }
+            )
         }
     }
 }
