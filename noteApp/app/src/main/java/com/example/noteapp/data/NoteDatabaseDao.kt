ecc49bd33f69d4bb9ba197ae05de99162cdaeac2
BartoszRudnik <rudnik49@gmail.com>
2022-05-22 22:58:05 +0200

    noteApp -> adding provider

---
+++ noteApp/app/src/main/java/com/example/noteapp/data/NoteDatabaseDao.kt
@@ -0,0 +1,27 @@
+package com.example.noteapp.data
+
+import androidx.room.*
+import com.example.noteapp.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDatabaseDao {
+
+    @Query("SELECT * from note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query("SELECT * from note where id =:id")
+    suspend fun getNote(id: String): Note
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addNote(note: Note)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateNote(note: Note)
+
+    @Query("DELETE from note")
+    suspend fun deleteAll()
+
+    @Query("DELETE from note where id =:id")
+    suspend fun deleteSingleNote(id: String)
+}
