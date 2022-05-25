b8343679ddad65a7b9418d34e302af4f581f16e5
BartoszRudnik <rudnik49@gmail.com>
2022-05-25 23:02:14 +0200

    jetTrivia -> question tracker widget

--- jetTrrivia/app/src/main/java/com/example/jettrrivia/components/Questions.kt
+++ jetTrrivia/app/src/main/java/com/example/jettrrivia/components/Questions.kt
@@ -1,7 +1,17 @@
 package com.example.jettrrivia.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.jettrrivia.screen.QuestionsViewModel
 
 @Composable
@@ -11,4 +21,27 @@ fun Questions(viewModel: QuestionsViewModel) {
     if (viewModel.data.value.loading == true) {
         CircularProgressIndicator()
     }
+}
+
+@Composable
+fun QuestionDisplay() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp), color = Color.Magenta
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            QuestionTracker()
+        }
+    }
+}
+
+@Composable
+fun QuestionTracker(counter: Int = 10, outOf: Int = 20) {
+    Text(text = "Question $counter / $outOf", modifier = Modifier.padding(20.dp))
+
 }
