8076098837e17c850789a5db1fd7eb0bd9aafeea
BartoszRudnik <rudnik49@gmail.com>
2022-05-26 15:30:47 +0200

    jetTriviaApp -> question ui

--- jetTrrivia/app/src/main/java/com/example/jettrrivia/components/Questions.kt
+++ jetTrrivia/app/src/main/java/com/example/jettrrivia/components/Questions.kt
@@ -1,17 +1,26 @@
 package com.example.jettrrivia.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jettrrivia.model.Question
 import com.example.jettrrivia.screen.QuestionsViewModel
 
 @Composable
@@ -20,11 +29,41 @@ fun Questions(viewModel: QuestionsViewModel) {
 
     if (viewModel.data.value.loading == true) {
         CircularProgressIndicator()
+    } else {
+        if (questions.isNullOrEmpty()) {
+            QuestionDisplay(question = questions!!.first())
+        }
     }
 }
 
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+    question: Question,
+    //questionIndex: MutableState<Int>,
+    //viewModel: QuestionsViewModel,
+    //onNextClicked: (Int) -> Unit
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
+
+    val answerState = remember(question) {
+        mutableStateOf<Int?>(null)
+    }
+
+    val correctAnswerState = remember(question) {
+        mutableStateOf<Boolean?>(null)
+    }
+
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+
+    val updateAnswer: (Int) -> Unit = remember(question) {
+        {
+            answerState.value = it
+            correctAnswerState.value = choicesState[it] == question.answer
+        }
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -36,6 +75,63 @@ fun QuestionDisplay() {
             horizontalAlignment = Alignment.Start
         ) {
             QuestionTracker()
+            DrawDottedLine(pathEffect = pathEffect)
+
+            Column {
+                Text(
+                    text = question.question,
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp,
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(0.3f)
+                )
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .border(
+                                width = 4.dp,
+                                shape = RoundedCornerShape(15.dp),
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        Color.Cyan,
+                                        Color.Cyan
+                                    )
+                                )
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    topEndPercent = 50,
+                                    bottomEndPercent = 50,
+                                    bottomStartPercent = 50,
+                                )
+                            )
+                            .background(Color.Transparent),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = answerState.value == index,
+                            onClick = { updateAnswer(index) },
+                            modifier = Modifier.padding(4.dp),
+                            colors = RadioButtonDefaults.colors(
+                                selectedColor = if (correctAnswerState.value == true && index == answerState.value) {
+                                    Color.Green.copy(alpha = 0.2f)
+                                } else {
+                                    Color.Red.copy(alpha = 0.2f)
+                                }
+                            )
+                        )
+                        Text(text = answerText)
+                    }
+                }
+            }
         }
     }
 }
@@ -43,5 +139,20 @@ fun QuestionDisplay() {
 @Composable
 fun QuestionTracker(counter: Int = 10, outOf: Int = 20) {
     Text(text = "Question $counter / $outOf", modifier = Modifier.padding(20.dp))
+}
 
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+    ) {
+        drawLine(
+            color = Color.White,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect
+        )
+    }
 }
