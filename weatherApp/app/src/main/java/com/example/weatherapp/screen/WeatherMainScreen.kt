d3052e27d1a481a7c56a9fae9d9a8a5bdace2cc3
BartoszRudnik <rudnik49@gmail.com>
2022-05-30 15:27:34 +0200

    weatherApp -> app bar ui

--- weatherApp/app/src/main/java/com/example/weatherapp/screen/WeatherMainScreen.kt
+++ weatherApp/app/src/main/java/com/example/weatherapp/screen/WeatherMainScreen.kt
@@ -1,33 +1,55 @@
 package com.example.weatherapp.screen
 
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.weatherapp.data.DataOrException
 import com.example.weatherapp.model.Weather
+import com.example.weatherapp.widgets.WeatherAppBar
 import java.lang.Exception
 
 @Composable
 fun WeatherMainScreen(navController: NavController, viewModel: MainViewModel) {
-    ShowData(viewModel)
-}
-
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
     val weatherData =
         produceState<DataOrException<Weather, Boolean, Exception>>(
             initialValue = DataOrException(
                 loading = true
             )
         ) {
-            value = mainViewModel.getWeatherData(city = "Seattle", units = "imperial")
+            value = viewModel.getWeatherData(city = "Seattle", units = "imperial")
         }.value
 
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Text(text = weatherData.data!!.city.country)
+        MainScaffold(weatherData.data!!, navController)
     }
+
+
+}
+
+@Composable
+fun MainScaffold(weatherData: Weather, navController: NavController) {
+    Scaffold(topBar = {
+        WeatherAppBar(
+            navController = navController,
+            title = weatherData.city.name + ", ${weatherData.city.country}",
+            elevation = 5.dp, icon = Icons.Default.ArrowBack
+        ) {
+
+        }
+    }) {
+        MainContent(weatherData = weatherData)
+    }
+}
+
+@Composable
+fun MainContent(weatherData: Weather) {
+    Text(text = weatherData.city.name)
 }
