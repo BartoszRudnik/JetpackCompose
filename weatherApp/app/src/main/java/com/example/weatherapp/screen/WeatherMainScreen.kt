cafc9a61774c2a5a3fec2540f480fc4cea75e3f4
BartoszRudnik <rudnik49@gmail.com>
2022-05-31 11:48:46 +0200

    weatherApp -> humidity, wind, pressure, sunset, sunrise

--- weatherApp/app/src/main/java/com/example/weatherapp/screen/WeatherMainScreen.kt
+++ weatherApp/app/src/main/java/com/example/weatherapp/screen/WeatherMainScreen.kt
@@ -11,17 +11,22 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
+import com.example.weatherapp.R
 import com.example.weatherapp.data.DataOrException
 import com.example.weatherapp.model.Weather
+import com.example.weatherapp.model.WeatherItem
 import com.example.weatherapp.util.formatDate
+import com.example.weatherapp.util.formatDateTime
 import com.example.weatherapp.util.formatDecimals
 import com.example.weatherapp.widgets.WeatherAppBar
 import java.lang.Exception
+import java.sql.Timestamp
 
 @Composable
 fun WeatherMainScreen(navController: NavController, viewModel: MainViewModel) {
@@ -94,6 +99,83 @@ fun MainContent(weatherData: Weather) {
                 Text(text = weatherData.list[0].weather[0].main, fontStyle = FontStyle.Italic)
             }
         }
+
+        HumidityWindPressureRow(weatherItem = weatherData.list[0])
+        Divider()
+        SunriseAndSunsetRow(weatherItem = weatherData.list[0])
+    }
+}
+
+@Composable
+fun SunriseAndSunsetRow(weatherItem: WeatherItem) {
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        SunriseSunsetRowItem(
+            value = weatherItem.sunrise,
+            iconId = R.drawable.sunrise,
+            contentDescription = "Sunrise icon"
+        )
+        SunriseSunsetRowItem(
+            value = weatherItem.sunset,
+            iconId = R.drawable.sunset,
+            contentDescription = "Sunset icon"
+        )
+    }
+}
+
+@Composable
+fun HumidityWindPressureRow(weatherItem: WeatherItem) {
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        HumidityRowItem(
+            iconId = R.drawable.humidity,
+            value = weatherItem.humidity,
+            contentDescription = "Humidity icon"
+        )
+        HumidityRowItem(
+            iconId = R.drawable.pressure,
+            value = weatherItem.pressure,
+            contentDescription = "Pressure icon"
+        )
+        HumidityRowItem(
+            iconId = R.drawable.wind,
+            value = weatherItem.speed.toInt(),
+            contentDescription = "Wind icon"
+        )
+    }
+}
+
+@Composable
+private fun SunriseSunsetRowItem(value: Int, iconId: Int, contentDescription: String) {
+    Row(modifier = Modifier.padding(4.dp)) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = contentDescription,
+            modifier = Modifier.size(20.dp)
+        )
+        Text(text = formatDateTime(value), style = MaterialTheme.typography.caption)
+    }
+}
+
+@Composable
+private fun HumidityRowItem(iconId: Int, value: Int, contentDescription: String) {
+    Row(modifier = Modifier.padding(4.dp)) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = contentDescription,
+            modifier = Modifier.size(20.dp)
+        )
+        Text(text = value.toString(), style = MaterialTheme.typography.caption)
     }
 }
 
