e69abd69c9701445e7e50fb79b497d1b4d7ce50f
BartoszRudnik <rudnik49@gmail.com>
2022-05-31 10:49:50 +0200

    weatherApp -> top circle

--- weatherApp/app/src/main/java/com/example/weatherapp/screen/WeatherMainScreen.kt
+++ weatherApp/app/src/main/java/com/example/weatherapp/screen/WeatherMainScreen.kt
@@ -1,16 +1,25 @@
 package com.example.weatherapp.screen
 
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.weatherapp.data.DataOrException
 import com.example.weatherapp.model.Weather
+import com.example.weatherapp.util.formatDate
+import com.example.weatherapp.util.formatDecimals
 import com.example.weatherapp.widgets.WeatherAppBar
 import java.lang.Exception
 
@@ -51,5 +60,48 @@ fun MainScaffold(weatherData: Weather, navController: NavController) {
 
 @Composable
 fun MainContent(weatherData: Weather) {
-    Text(text = weatherData.city.name)
+    val imageUrl = "https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}.png"
+
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = formatDate(weatherData.list[0].dt),
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary, fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp), shape = CircleShape, color = Color.Yellow
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(
+                    text = formatDecimals(weatherData.list[0].temp.day) + "°",
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(text = weatherData.list[0].weather[0].main, fontStyle = FontStyle.Italic)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    Image(
+        painter = rememberImagePainter(imageUrl),
+        contentDescription = "Weather state icon",
+        modifier = Modifier.size(80.dp)
+    )
 }
