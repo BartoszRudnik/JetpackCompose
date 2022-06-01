480f80dd6651b2ec18e07464b75d5f7fa376454f
BartoszRudnik <rudnik49@gmail.com>
2022-06-01 15:20:05 +0200

    weatherApp -> add weather db repository

--- weatherApp/app/src/main/java/com/example/weatherapp/di/AppModule.kt
+++ weatherApp/app/src/main/java/com/example/weatherapp/di/AppModule.kt
@@ -1,19 +1,38 @@
 package com.example.weatherapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.weatherapp.data.WeatherDao
+import com.example.weatherapp.data.WeatherDatabase
 import com.example.weatherapp.network.WeatherApi
 import com.example.weatherapp.util.Constants
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+    @Singleton
+    @Provides
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao =
+        weatherDatabase.weatherDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): WeatherDatabase =
+        Room.databaseBuilder(context, WeatherDatabase::class.java, "weather_db")
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Singleton
     @Provides
     fun provideOpenWeatherApi(): WeatherApi {
         return Retrofit.Builder().baseUrl(Constants.BASE_URL)
---
+++ weatherApp/app/src/main/java/com/example/weatherapp/repository/WeatherDBRepository.kt
@@ -0,0 +1,15 @@
+package com.example.weatherapp.repository
+
+import com.example.weatherapp.data.WeatherDao
+import com.example.weatherapp.model.Favorite
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class WeatherDBRepository @Inject constructor(private val weatherDao: WeatherDao) {
+    fun getFavorites(): Flow<List<Favorite>> = weatherDao.getFavorites()
+    suspend fun insertFavorite(favorite: Favorite) = weatherDao.insertFavorite(favorite)
+    suspend fun updateFavorite(favorite: Favorite) = weatherDao.updateFavorite(favorite)
+    suspend fun deleteAllFavorites() = weatherDao.deleteAllFavorites()
+    suspend fun deleteById(city: String) = weatherDao.deleteFavById(city)
+    suspend fun getFavoriteById(city: String) = weatherDao.getFavById(city)
+}
