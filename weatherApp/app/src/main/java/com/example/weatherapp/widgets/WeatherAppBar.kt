0046ddb6cf89eebcd98b50fdd629edf9db1e38fb
BartoszRudnik <rudnik49@gmail.com>
2022-06-01 11:36:31 +0200

    weatherApp -> dialog & dropdown menu

--- weatherApp/app/src/main/java/com/example/weatherapp/widgets/WeatherAppBar.kt
+++ weatherApp/app/src/main/java/com/example/weatherapp/widgets/WeatherAppBar.kt
@@ -1,12 +1,17 @@
 package com.example.weatherapp.widgets
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -27,6 +32,14 @@ fun WeatherAppBar(
     onAddActionClicked: () -> Unit = {},
     onButtonClicked: () -> Unit = {}
 ) {
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+
+    if (showDialog.value) {
+        ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
+    }
+
     TopAppBar(
         title = {
             Text(
@@ -65,4 +78,48 @@ fun WeatherAppBar(
         backgroundColor = Color.Transparent,
         elevation = elevation
     )
+}
+
+@Composable
+fun ShowSettingDropDownMenu(showDialog: MutableState<Boolean>, navController: NavController) {
+    var expanded by remember {
+        mutableStateOf(true)
+    }
+    var items = listOf<String>("About", "Favorites", "Settings")
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .absolutePadding(top = 45.dp, right = 20.dp)
+    ) {
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White)
+        ) {
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }) {
+                    Icon(
+                        imageVector = when (text) {
+                            "About" -> Icons.Default.Info
+                            "Favorites" -> Icons.Default.Favorite
+                            else -> Icons.Default.Settings
+                        }, contentDescription = null,
+                        tint = Color.LightGray
+                    )
+                    Text(
+                        text = text,
+                        modifier = Modifier.clickable { },
+                        fontWeight = FontWeight.W300
+                    )
+                }
+            }
+        }
+    }
 }
