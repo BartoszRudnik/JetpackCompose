28de8ccf18802304a49f552e196b2859e2770f92
BartoszRudnik <rudnik49@gmail.com>
2022-06-07 15:50:24 +0200

    readerApp -> creating user data in firestore db

--- readerApp/app/src/main/java/com/example/myapplication/screens/login/LoginScreenViewModel.kt
+++ readerApp/app/src/main/java/com/example/myapplication/screens/login/LoginScreenViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
@@ -19,21 +20,59 @@ class LoginScreenViewModel : ViewModel() {
 
     val loading: LiveData<Boolean> = _loading
 
+    private fun createUser(displayName: String?) {
+        val userId = auth.currentUser?.uid
+        val user = mutableMapOf<String, Any>()
+
+        user["user_id"] = userId.toString()
+        user["display_name"] = displayName.toString()
+
+        FirebaseFirestore.getInstance().collection("users").add(user)
+    }
+
     fun createUserWithEmailAndPassword(email: String, password: String, home: () -> Unit) =
-        viewModelScope.launch {}
+        viewModelScope.launch {
+            try {
+                if (_loading.value == false) {
+                    _loading.value = true
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val displayName = task.result?.user?.email?.split("@")?.get(0)
+                                createUser(displayName)
+
+                                home()
+                            } else {
+
+                            }
+
+                            _loading.value = false
+                        }
+                }
+            } catch (ex: Exception) {
+
+            }
+
+        }
 
     fun loginUserWithEmailAndPassword(email: String, password: String, home: () -> Unit) =
         viewModelScope.launch {
             try {
+                if (_loading.value == false) {
+                    _loading.value = true
 
-            } catch (ex: Exception) {
-                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        home()
-                    } else {
+                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            home()
+                        } else {
 
+                        }
+
+                        _loading.value = false
                     }
                 }
+            } catch (ex: Exception) {
+
             }
         }
 }
