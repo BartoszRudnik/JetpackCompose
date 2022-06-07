13edc70efe3401c262faf4a2b54c52749191df18
BartoszRudnik <rudnik49@gmail.com>
2022-06-07 13:52:31 +0200

    readerApp -> finished login form ui

--- readerApp/app/src/main/java/com/example/myapplication/screens/login/ReaderLoginScreen.kt
+++ readerApp/app/src/main/java/com/example/myapplication/screens/login/ReaderLoginScreen.kt
@@ -1,15 +1,16 @@
 package com.example.myapplication.screens.login
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Class
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -19,107 +20,56 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.myapplication.components.UserForm
 import com.example.myapplication.screens.AReaderText
 import java.time.format.TextStyle
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ReaderLoginScreen(navController: NavController) {
+    val showLoginForm = rememberSaveable {
+        mutableStateOf(true)
+    }
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
             AReaderText()
-            UserForm()
+
+            if (showLoginForm.value) UserForm() { email, password ->
+            }
+            else UserForm(loading = false, isCreateAccount = true) { email, password ->
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+            Row(
+                modifier = Modifier.padding(15.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val text = if (showLoginForm.value) "Sign Up" else "Login"
+
+                Text(text = "New User?")
+                Text(text = text,
+                    modifier = Modifier
+                        .clickable {
+                            showLoginForm.value = !showLoginForm.value
+                        }
+                        .padding(4.dp),
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colors.secondaryVariant)
+            }
         }
     }
 }
 
-@ExperimentalComposeUiApi
-@Composable
-fun UserForm() {
-    val email = rememberSaveable {
-        mutableStateOf("")
-    }
-    val password = rememberSaveable {
-        mutableStateOf("")
-    }
-    val passwordVisibility = rememberSaveable {
-        mutableStateOf(false)
-    }
-    val passwordFocusRequest = FocusRequester.Default
-    val keyboardController = LocalSoftwareKeyboardController.current
-    val valid = remember(email.value, password.value) {
-        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
-    }
-
-    val modifier = Modifier
-        .height(250.dp)
-        .background(MaterialTheme.colors.background)
-        .verticalScroll(
-            rememberScrollState()
-        )
-
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        EmailInput(emailState = email, enabled = true, onAction = KeyboardActions {
-            passwordFocusRequest.requestFocus()
-        })
-    }
-}
-
-@Composable
-fun EmailInput(
-    modifier: Modifier = Modifier,
-    emailState: MutableState<String>,
-    labelId: String = "Email",
-    enabled: Boolean = true,
-    imeAction: ImeAction = ImeAction.Next,
-    onAction: KeyboardActions = KeyboardActions.Default
-) {
-    InputField(
-        modifier = modifier,
-        valueState = emailState,
-        labelId = labelId,
-        enabled = enabled,
-        keyboardType = KeyboardType.Email,
-        imeAction = imeAction,
-        onAction = onAction,
-    )
-}
-
-@Composable
-fun InputField(
-    modifier: Modifier = Modifier,
-    valueState: MutableState<String>,
-    labelId: String,
-    enabled: Boolean,
-    isSingleLine: Boolean = true,
-    keyboardType: KeyboardType = KeyboardType.Text,
-    imeAction: ImeAction = ImeAction.Next,
-    onAction: KeyboardActions = KeyboardActions.Default
-) {
-    OutlinedTextField(
-        value = valueState.value,
-        onValueChange = {
-            valueState.value = it
-        },
-        label = { Text(text = labelId) },
-        singleLine = isSingleLine,
-        textStyle = androidx.compose.ui.text.TextStyle(
-            fontSize = 18.sp,
-            color = MaterialTheme.colors.onBackground
-        ),
-        modifier = modifier
-            .padding(10.dp)
-            .fillMaxWidth(),
-        enabled = enabled,
-        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction)
-    )
-}
